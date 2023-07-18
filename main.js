6534ee79c9511f32c121dc271a3bfb04fcd0faf0
RichardENeumann <RichardNeumann@googlemail.com>
2023-07-18 13:49:14 +0200

    Add main game logic

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 "use strict";
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
+const playerSelection = "rOcK";
 
 function getComputerChoice() {
-    let randomSeed = Math.floor(Math.random() * 10) % 3 + 1;
-    switch(randomSeed) {
+    let randomChoice = Math.floor(Math.random() * 10) % 3 + 1;
+    switch(randomChoice) {
         case 1:
             return "Rock";
             break;
@@ -19,5 +18,47 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
+    if (playerSelection === computerSelection) {
+        return playerSelection + " versus " + computerSelection + " - That's a draw!";
+    } else if (playerSelection === "Rock") {
+        switch (computerSelection) {
+            case "Paper":
+                return "Paper beats rock! - The computer wins!";
+                break;
+            case "Scissors":
+                return "Rock beats scissors! - You win!";
+                break;  
+            default: 
+                return "Something went wrong here, try again!";                  
+                break;
+        }
+    } else if (playerSelection === "Paper") {
+        switch (computerSelection) {
+            case "Rock":
+                return "Paper beats rock! - You win!";
+                break;
+            case "Scissors":
+                return "Scissors beat paper! - The computer wins!";
+                break;
+            default: 
+                return "Something went wrong here, try again!";                  
+                break;                  
+        }                          
+    } else if (playerSelection === "Scissors") {
+        switch (computerSelection) {
+            case "Rock":
+                return "Rock beats scissors - The computer wins!";
+                break;
+            case "Paper":
+                return "Scissors beat paper! - You win!";
+                break;                    
+            default: 
+                return "Something went wrong here, try again!";                  
+                break;    
+        }        
+    } else {
+        return "Something went wrong here, try again!";
+    }
 }
