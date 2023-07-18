20c2335cd0d1e00ad168f0b0e548041a55c41b95
RichardENeumann <RichardNeumann@googlemail.com>
2023-07-18 13:09:28 +0200

    Add getComputerChoice function

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 "use strict";
 
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+function getComputerChoice() {
+    let randomSeed = Math.floor(Math.random() * 10) % 3 + 1;
+    switch(randomSeed) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;    
+        case 3:
+            return "Scissors";
+            break;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
