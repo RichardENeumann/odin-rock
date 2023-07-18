878ff7b691912334aeaab8c8b88374817c27fd81
RichardENeumann <RichardNeumann@googlemail.com>
2023-07-18 16:01:20 +0200

    Implement score keeping

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 10) % 3 + 1;
     switch(randomChoice) {
@@ -23,9 +26,11 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "Rock") {
         switch (computerSelection) {
             case "Paper":
+                computerScore++;
                 return "Paper beats rock! - The computer wins!";
                 break;
             case "Scissors":
+                playerScore++;
                 return "Rock beats scissors! - You win!";
                 break;  
             default: 
@@ -35,9 +40,11 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "Paper") {
         switch (computerSelection) {
             case "Rock":
+                playerScore++;
                 return "Paper beats rock! - You win!";
                 break;
             case "Scissors":
+                computerScore++;
                 return "Scissors beat paper! - The computer wins!";
                 break;
             default: 
@@ -47,9 +54,11 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "Scissors") {
         switch (computerSelection) {
             case "Rock":
+                computerScore++;
                 return "Rock beats scissors - The computer wins!";
                 break;
             case "Paper":
+                playerScore++;
                 return "Scissors beat paper! - You win!";
                 break;                    
             default: 
@@ -70,8 +79,20 @@ function playGame(numberOfRounds) {
 }
 
 function mainLoop() {
-    let numberOfRounds = prompt("How many rounds would you like to play?");
-    return playGame(numberOfRounds);
+    console.log(playGame(5));
+    console.log("Your score:" + playerScore + "|| The computer's score:" + computerScore);
+    let result = Math.sign(playerScore - computerScore);
+    switch(result) {
+        case -1:
+            console.log("The Computer won!");
+            break;
+        case 1:
+            console.log("You won! Yay!");
+            break;
+        default:
+            console.log("It's a tie!");
+            break;
+    }
 }
 
 mainLoop();
